2aff04fc48f4fd9e50282ca1a908630ad8e1a31a
rheinaadinda <moranisinurat@gmail.com>
2025-09-09 20:05:41 +0700

    Added URL Deployment PWS

--- strike_apparel/settings.py
+++ strike_apparel/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rheina-adinda-strikeapparel.pbp.cs.ui.ac.id"]
 
 
 # Application definition
